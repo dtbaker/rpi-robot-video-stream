c0f73c46c7b953b52c70f6080d382227667e6446
dtbaker <dtbaker@gmail.com>
2020-08-23 21:25:03 +1000

    some config vars for server addresses

--- server.js
+++ server.js
@@ -2,30 +2,35 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http)
 
+const config = {
+  tcpServerPort: 9999,
+  webServerPort: 3000
+}
+
 // This stuff accepts the inbound ffmpeg TCP stream and sends the data out to any connected websocket clients.
 const net = require('net')
 let firstPackets = []
 const tcpServer = net.createServer(tcpSocket => {
-  tcpSocket.on('end', function() {
+  tcpSocket.on('end', function () {
     console.log('streamer disconnected');
     firstPackets = []
   });
   tcpSocket.on('data', data => {
     // Capture first few important frames which are required to be sent to any newly connected clients.
-    if(firstPackets.length < 5){
+    if (firstPackets.length < 5) {
       firstPackets.push(data)
     }
     // This sends the video frame out to any connected clients:
     io.emit('video', data)
   })
 })
-tcpServer.listen(9999, () => {
-  console.log('tcp server started, ready to accept stream data');
+tcpServer.listen(config.tcpServerPort, () => {
+  console.log(`tcp server started, ready to accept stream data on port ${config.tcpServerPort}`);
 })
 
 // Basic web app with HTML page that loads video preview and websocket UI stuff
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/server.html');
 });
 let clientCount = 0
 io.on('connection', (socket) => {
@@ -37,11 +42,11 @@ io.on('connection', (socket) => {
   })
   socket.on('disconnect', () => {
     clientCount--
-    if(clientCount<0)clientCount = 0 // should never happen, but whatevs
+    if (clientCount < 0) clientCount = 0 // should never happen, but whatevs
     io.emit('clientCount', clientCount)
   });
 });
 
-http.listen(3000, () => {
-  console.log('Open your browser to http://localhost:3000 ');
+http.listen(config.webServerPort, () => {
+  console.log(`Open your browser to http://localhost:${config.webServerPort}`);
 });
