706e127d2a9777f459cdecae6fd5f14e96d26356
dtbaker <dtbaker@gmail.com>
2020-08-25 21:38:26 +1000

    Fix up motor directions/speed

--- pi-robot/pi.js
+++ pi-robot/pi.js
@@ -133,22 +133,70 @@ raspi.init(() => {
   })
 })
 
-const motorSpeed = 14
-const turnSpeed = 14
+// 01 = reverse
+// 10 = forward
+
+// Set Motor 1:
+// C0 = 11000000 brake low
+// C1 = 11000001 reverse
+// C2 = 11000010 forward
+// C3 = 11000011 brake low
+// Set Motor 2
+// C8 = 11001000 brake low
+// C9 = 11001001 forward
+// CA = 11001010 forward
+// CB = 11001011 brake low
+// Set both Motor 1 and 2
+// D1 = 11010001 m1 reverse
+// D2 = 11010010 m1 forward
+// D4 = 11010100 m2 reverse
+// D8 = 11011000 m2 forward
+// D5 = 11010101 m1 reverse, m2 reverse
+// DA = 11011010 m1 forward, m2 forward
+// D9 = 11011001 m1 reverse, m2 forward
+// D6 = 11010110 m1 forward, m2 reverse
+// Accelerate both Motor 1 and 2
+// E1 = 11010001 m1 reverse
+// E2 = 11010010 m1 forward
+// E4 = 11010100 m2 reverse
+// E8 = 11011000 m2 forward
+// E5 = 11010101 m1 reverse, m2 reverse
+// EA = 11011010 m1 forward, m2 forward
+// E9 = 11011001 m1 reverse, m2 forward
+// E6 = 11010110 m1 forward, m2 reverse
+// Accelerate Motor 1:
+// C4 = 11000100 brake low
+// C5 = 11000101 reverse
+// C6 = 11000110 forward
+// C7 = 11000111 brake low
+// Accelerate Motor 2:
+// CC = 11001100 brake low
+// CD = 11001101 reverse
+// CE = 11001110 forward
+// CF = 11001111 brake low
+
+const motorSpeed = 25
+const turnSpeed = 22
+
+// DOH! My motor 1 was wired in reverse. Fixing that in software.
 
 async function moveThisThing() {
   if (currentDirection && serial) {
     switch (currentDirection) {
       case 'left':
-        serial.write(Buffer.from([0xEA, turnSpeed, turnSpeed]))
+        // move both forward (m1 wiring is reversed oops) but make right wheel spin faster
+        serial.write(Buffer.from([0xE9, turnSpeed / 2, turnSpeed]))
         break;
       case 'right':
-        serial.write(Buffer.from([0xE5, turnSpeed, turnSpeed]))
+        // move both forward (m1 wiring is reversed oops) but make right wheel spin faster
+        serial.write(Buffer.from([0xE9, turnSpeed, turnSpeed / 2]))
         break;
       case 'forward':
+        // move both forward (m1 wiring is reversed oops)
         serial.write(Buffer.from([0xE9, motorSpeed, motorSpeed]))
         break;
       case 'back':
+        // move both back (m1 wiring is reversed oops)
         serial.write(Buffer.from([0xE6, motorSpeed, motorSpeed]))
         break;
     }
